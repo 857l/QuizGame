52d374272ee455cf783150d48733da9079d2968a
857l <nik.gusev.2017@bk.ru>
2025-09-11 23:28:51 +0300

    pattern observe

---
+++ app/src/main/java/ru/n857l/quizgame/load/LoadScreen.kt
@@ -0,0 +1,5 @@
+package ru.n857l.quizgame.load
+
+import ru.n857l.quizgame.core.Screen
+
+object LoadScreen : Screen.Replace(LoadFragment::class.java)
---
+++ app/src/main/java/ru/n857l/quizgame/load/UiObservable.kt
@@ -0,0 +1,37 @@
+package ru.n857l.quizgame.load
+
+interface UiObservable {
+
+    fun register(observer: (LoadUiState) -> Unit)
+
+    fun unregister()
+
+    fun postUiState(uiState: LoadUiState)
+
+    class Base : UiObservable {
+
+        private var uiStateCached: LoadUiState? = null
+        private var observerCached: ((LoadUiState) -> Unit)? = null
+
+        override fun register(observer: (LoadUiState) -> Unit) {
+            observerCached = observer
+            if (uiStateCached != null) {
+                observerCached!!.invoke(uiStateCached)
+                uiStateCached = null
+            }
+        }
+
+        override fun unregister() {//onPause
+            observerCached = null
+        }
+
+        override fun postUiState(uiState: LoadUiState) {
+            if (observerCached == null) {
+                uiStateCached = uiState
+            } else {
+                observerCached!!.invoke(uiState)
+                uiStateCached = null
+            }
+        }
+    }
+}
